a276981d21758f953e6e3607e39e2c905c5c8944
Daniel Dickson <ninjadd@gmail.com>
2017-10-26 12:31:47 -0700

    crated UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create(['name' => 'Daniel Dickson', 'email' => 'ninjadd@gmail.com', 'password' => bcrypt('6891ninja'), 'role' => 'admin']);
+
+        $faker = Faker::create();
+
+        foreach (range(1, 49) as $item) {
+            $user = new User();
+
+            $user->name = $faker->name();
+            $user->email = $faker->email;
+            $user->password = bcrypt('secret');
+            $user->role = $faker->randomElement(['owner', 'staff']);
+            $user->save();
+        }
     }
 }
